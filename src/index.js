a7d45a0b50b8b86647be4f6e734f3d4cf0edd7f1
Marian Steinbach <marian@giantswarm.io>
2018-09-20 10:02:50 +0200

    Add status info component showing the date range

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import results from './spider_result.json';
 import screenshots from './screenshots.json';
+import StatusInfo from './StatusInfo';
 import ResultsTable from './ResultsTable';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ResultsTable results={results} screenshots={screenshots}/>, document.getElementById('root'));
+ReactDOM.render(<StatusInfo results={results}/>, document.getElementById('status'));
 registerServiceWorker();
