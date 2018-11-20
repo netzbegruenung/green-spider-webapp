7f31b748214f9020df2227ab723bee8a8ffcb4cd
Marian Steinbach <marian@giantswarm.io>
2018-11-20 21:55:20 +0100

    Fix IDN URL display (#16)
    * Extract URL display into new component URLField
    
    * Adjust CSS
    
    * Remove unused import

--- src/SiteDetailsPage.js
+++ src/SiteDetailsPage.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import LocationLabel from './LocationLabel';
 import ScoreField from './ScoreField';
 import { TypeField, StateField } from './LocationLabel';
+import URLField from './URLField';
 import './SiteDetailsPage.css';
 import axios from 'axios';
-import punycode from 'punycode';
 
 
 class SiteDetailsPage extends Component {
@@ -116,7 +116,7 @@ class SiteDetailsPage extends Component {
                          state={this.state.site.meta.state} />
           </h1>
 
-          <p><SiteIcon site={this.state.site} /> <a href={ this.state.url } rel='noopener noreferrer' target='_blank'>{ punycode.toUnicode(this.state.url) }</a></p>
+          <p><SiteIcon site={this.state.site} /> <URLField url={ this.state.url } showScheme={true} showPath={true} /></p>
 
           <hr />
 
