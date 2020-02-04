c39a9e182197cecee2c37eb48646165716b03f7a
Marian Steinbach <marian@sendung.de>
2020-02-04 21:37:14 +0100

    ResponsiveField Text update (#88)

--- src/ui/criterium_fields/ResponsiveField.js
+++ src/ui/criterium_fields/ResponsiveField.js
@@ -11,10 +11,10 @@ class ResponsiveField extends Component {
   
       return <CriteriumField keyProp='responsive' type='negative' title='Mobile Endgeräte sollten unterstützt werden'>
           <p>Immer mehr Menschen nutzen das Web über ein mobiles Endgerät wie z. B. ein Smartphone. Die Site sollte
-            dies unterstützen, indem das Layout sich <em>"responsive"</em> an das Endgerät anpasst.</p>
+            dies unterstützen, indem das Layout sich <em>responsiv</em> an das Endgerät anpasst.</p>
           <p>Green Spider testet, wie breit die Startseite der Site auf verschieden breiten Bildschirmen ausfällt.
             Ist in einer Breite die Seite breiter als der Bildschirm, so gilt der Test als nicht bestanden.
-            Nutzer*innen con Smartphones sehen in diesen Fällen häufig einen horizontalen Scrollbalken oder müssen
+            Nutzer*innen von Smartphones sehen in diesen Fällen häufig einen horizontalen Scrollbalken oder müssen
             zum vollständigen Betrachten der Seite die Inhalte horizontal Verschieben.</p>
   
           <p>Tipp: Zieh den Browserfenster so schmal wie Du kannst, im besten Fall auf 360 Pixel Breite. Damit erhältst
